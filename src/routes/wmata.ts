bc9c761899736a70359ad1c067a5b9ef8ec4e94f
Ivan Diaz <idiaz3@gmu.edu>
2023-01-02 21:13:02 -0500

    added type

--- src/routes/wmata.ts
+++ src/routes/wmata.ts
@@ -5,7 +5,7 @@ import  { getStationInfo }  from "../controllers/wmata.ts";
 const router = express.Router();
 
 router.get('/stationInfo/:station', getStationInfo);
-router.get('/', (_req , res : Response): void =>{
+router.get('/', (_req : Request, res : Response): void =>{
     res.status(404).send('route not found');
 });
 export default router;
