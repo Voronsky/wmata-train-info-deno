f7583a07e9c8b83c0b9b133a35a6eb8a1ed2b1d9
Ivan Diaz <idiaz3@gmu.edu>
2023-01-03 23:37:50 -0500

    added auth middleware and a login route

--- src/routes/wmata.ts
+++ src/routes/wmata.ts
@@ -1,10 +1,12 @@
 import express from 'npm:express@4.18.2';
 import {Response} from 'npm:express@4.18.2';
 import  { getStationInfo }  from "../controllers/wmata.ts";
+import {generateToken, auth} from '../../util/auth.ts'
 
 const router = express.Router();
 
-router.get('/stationInfo/:station', getStationInfo);
+router.post('/login', generateToken);
+router.get('/stationInfo/:station', auth, getStationInfo);
 router.get('/', (_req : Request, res : Response): void =>{
     res.status(404).send('route not found');
 });
